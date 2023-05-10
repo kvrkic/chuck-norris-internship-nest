ce31e2adebbd47dcca86d9e8b35f964b88bec2d4
Karlo Vrkić <kvrkic@profico.hr>
2023-05-10 14:02:49 +0200

    Add validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   readonly firstName: string;
+
+  @IsNotEmpty()
   readonly lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
